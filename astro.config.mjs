d0c6d7598f4e215980a56b0466c90338b2b41954
Brandon Williams <prettyboypizzawebsite@gmail.com>
2025-07-22 14:24:15 -0700

    Update astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://prettyboypizza.com',
+  base: "/",
   integrations: [react()],
   vite: {
     resolve: {
